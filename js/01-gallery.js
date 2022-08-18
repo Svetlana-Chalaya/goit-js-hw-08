ab1068af28fa01aa7a9b1b4a7702ac0764001ad8
Svetlana-Chalaya <as-torg@ua.fm>
2022-08-18 15:32:32 +0300

    add file gitignore

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from "./gallery-items";
 // Change code below this line
 
 console.log(galleryItems);
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const SimpleLightbox = require("simplelightbox");
+// Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+const cardsGallery = containerGalleryCardsItems(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", cardsGallery);
+
+function containerGalleryCardsItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return ` <li class="gallery__item">
+          <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title = "how long takes the slide animation"/>
+</a>
+</li>`;
+    })
+    .join("");
+}
+console.log(galleryItems);
+
+const lightbox = new SimpleLightbox(".gallery a", {});
+
+lightbox.show();
--- js/02-video.js
+++ js/02-video.js
@@ -1,1 +1,11 @@
+import player from "@vimeo/player";
 
+const vimeoPlayer = require("@vimeo/player");
+
+const onPlay = function (data) {
+  duration: 61.857;
+  percent: 0.049;
+  seconds: 3.034;
+};
+player.on("play", onPlay);
+console.log(onPlay);
