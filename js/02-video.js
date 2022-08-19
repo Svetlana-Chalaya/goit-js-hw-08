5eae0441bcc6e0ee051e4c8a89742a76544d7227
Svetlana-Chalaya <as-torg@ua.fm>
2022-08-19 19:05:36 +0300

    standard settings

--- js/02-video.js
+++ js/02-video.js
@@ -6,6 +6,23 @@ const onPlay = function (data) {};
 player.on("play", onPlay);
 console.log(onPlay);
 
+player
+  .setCurrentTime(30.456)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case "RangeError":
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+
 //  duration: 61.857;
 //  percent: 0.049;
 //  seconds: 3.034;
