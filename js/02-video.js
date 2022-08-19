9cedb7aeefd29be02eade19d4936b4a34ba4f512
Svetlana-Chalaya <as-torg@ua.fm>
2022-08-19 16:34:50 +0300

    task-02

--- js/02-video.js
+++ js/02-video.js
@@ -2,10 +2,10 @@ import player from "@vimeo/player";
 
 const vimeoPlayer = require("@vimeo/player");
 
-const onPlay = function (data) {
-  duration: 61.857;
-  percent: 0.049;
-  seconds: 3.034;
-};
+const onPlay = function (data) {};
 player.on("play", onPlay);
 console.log(onPlay);
+
+//  duration: 61.857;
+//  percent: 0.049;
+//  seconds: 3.034;
