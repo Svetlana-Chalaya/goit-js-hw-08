df7ea5b13b91ac7c3c53f918e09a44fd5b6d1466
Svetlana-Chalaya <as-torg@ua.fm>
2022-08-20 17:40:53 +0300

    localStorage

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,6 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,24 @@
-import player from "@vimeo/player";
+import Player from '@vimeo/player';
+// import lodash.throttle from lodash.throttle;
 
-const vimeoPlayer = require("@vimeo/player");
+const player = new Player('handstick', {
+  id: 19231868,
+  width: 640,
+});
 
-const onPlay = function (data) {};
-player.on("play", onPlay);
-console.log(onPlay);
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+const onPlay = function (data) {
+  {
+    duration: 61.857;
+    percent: 0.049;
+    seconds: 3.034;
+  }
+};
+
+player.on('play', onPlay);
 
 player
   .setCurrentTime(30.456)
@@ -13,7 +27,7 @@ player
   })
   .catch(function (error) {
     switch (error.name) {
-      case "RangeError":
+      case 'RangeError':
         // the time was less than 0 or greater than the video’s duration
         break;
 
@@ -23,6 +37,32 @@ player
     }
   });
 
-//  duration: 61.857;
-//  percent: 0.049;
-//  seconds: 3.034;
+if (window.localStorage) {
+  localStorage.setItem('videoplayer-current-time');
+  alert(typeof localStorage['videoplayer-current-time']);
+} else alert(' localStorage не підтримує ваш браузер ');
+
+// const vimeoPlayer = require('@vimeo/player');
+
+// const onPlay = function (data) {};
+// player.on('play', onPlay);
+// console.log(onPlay);
+
+// player
+//   .setCurrentTime(30.456)
+//   .then(function (seconds) {
+//     // seconds = the actual time that the player seeked to
+//   })
+//   .catch(function (error) {
+//     switch (error.name) {
+//       case 'RangeError':
+//         // the time was less than 0 or greater than the video’s duration
+//         break;
+
+//       default:
+//         duration: 61.857;
+//         percent: 0.049;
+//         seconds: 3.034;
+//         break;
+//     }
+//   });
