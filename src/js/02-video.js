e31081bf3ef649be1e2eb8297bbc43ed9d8bccfc
Svetlana-Chalaya <as-torg@ua.fm>
2022-08-21 19:04:11 +0300

    task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,68 +1,15 @@
 import Player from '@vimeo/player';
-// import lodash.throttle from lodash.throttle;
+import throttle from 'lodash.throttle';
 
-const player = new Player('handstick', {
-  id: 19231868,
-  width: 640,
-});
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+const STORAGE_KEY = 'videoplayer-current-time';
 
-const onPlay = function (data) {
-  {
-    duration: 61.857;
-    percent: 0.049;
-    seconds: 3.034;
-  }
-};
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY) || 0);
 
-player.on('play', onPlay);
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
 
-player
-  .setCurrentTime(30.456)
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
-
-      default:
-        // some other error occurred
-        break;
-    }
-  });
-
-if (window.localStorage) {
-  localStorage.setItem('videoplayer-current-time');
-  alert(typeof localStorage['videoplayer-current-time']);
-} else alert(' localStorage не підтримує ваш браузер ');
-
-// const vimeoPlayer = require('@vimeo/player');
-
-// const onPlay = function (data) {};
-// player.on('play', onPlay);
-// console.log(onPlay);
-
-// player
-//   .setCurrentTime(30.456)
-//   .then(function (seconds) {
-//     // seconds = the actual time that the player seeked to
-//   })
-//   .catch(function (error) {
-//     switch (error.name) {
-//       case 'RangeError':
-//         // the time was less than 0 or greater than the video’s duration
-//         break;
-
-//       default:
-//         duration: 61.857;
-//         percent: 0.049;
-//         seconds: 3.034;
-//         break;
-//     }
-//   });
+function onTimeUpdate(params) {
+  localStorage.setItem(STORAGE_KEY, params.seconds);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,1 @@
-
+// import lo
