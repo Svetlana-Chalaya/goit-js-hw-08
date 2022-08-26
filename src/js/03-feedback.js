a6c97d354634ae59d9e831b36df888c37c2c1eec
Svetlana-Chalaya <as-torg@ua.fm>
2022-08-26 12:45:19 +0300

    task-03 lodash.throttle

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,3 +1,9 @@
 import throttle from 'lodash.throttle';
 
 player.on('timeupdate', throttle(onTimeUpdate, 500));
+
+const STORAGE_KEY = 'feedback-form-state';
+
+function onTimeUpdate(params) {
+  localStorage.setItem(STORAGE_KEY);
+}
